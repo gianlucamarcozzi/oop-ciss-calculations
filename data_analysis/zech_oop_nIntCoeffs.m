7a4815b292fcb3b40934af13aae6a0fe0139ad56
gianlucamarcozzi <gmarcozzi@gmx.de>
2024-12-19 17:35:17 +0100

    Step forward for coeff oop-eseem vs beta

--- data_analysis/zech_oop_nIntCoeffs.m
+++ data_analysis/zech_oop_nIntCoeffs.m
@@ -20,7 +20,6 @@ nPhi = 91;
 nSolidAngle = nTheta*nPhi;
 
 rVers = ang2vec(ones(nTheta, 1)*phis, thetas*ones(1, nPhi));
-size(rVers)
 % Effective g-values
 g1Tensor = rotatematrixeuangles(diag(Sys.g(1, :)), Sys.gFrame(1, :));
 g1 = sqrt( sum( (g1Tensor*rVers).^2, 1));
@@ -29,15 +28,42 @@ g2 = sqrt( sum( (g2Tensor*rVers).^2, 1));
 % Dipolar interaction
 zD = erot(Sys.eeFrame)*[0, 0, 1]';
 dd = dipinteraction(dip, rVers, zD);
+deltaw = bmagn*B0/hbar*(g1 - g2);
+xi = mixinganglexi(deltaw, JJ*1e6, dd*1e6);
 
-for ii = 1:nSolidAngle
-    dipolar = dd(ii);
-    % angTheta = thetas(ii);
-    % angPhi = phis(ii);
-    gvalue1 = g1(ii);
-    gvalue2 = g2(ii);
-    xi = atan((dipolar + 2*JJ)/...
-        (bmagn*B0/hbar*(gvalue1 - gvalue2)));
+imagesc(reshape(xi, [nTheta, nPhi]))
+colorbar
 
-    integral2(@(x,y) x.^2 + y.^2, 0, 1, 0, 1);
+cBeta0 = @(tau) sin(thetas)'.*sin(2*(JJ - dd).*tau).*sin(2*xi).^2;
+cTwoBeta0 = @(tau) sin(thetas)'.*sin(2*(JJ - dd).*tau).*cos(xi).^4;
+
+nTau = 1000;
+taus = linspace(0, 10, nTau);
+cBeta = zeros(1, nTau);
+cTwoBeta = zeros(1, nTau);
+cBetaN = zeros(1, nTau);
+cTwoBetaN = zeros(1, nTau);
+for ii = 1:nTau
+    cBetaN(ii) = averageoversolidangle(cBeta0(taus(ii)), 91, 91, 2);
+    cTwoBetaN(ii) = averageoversolidangle(cTwoBeta0(taus(ii)), 91, 91, 2);
+    cTwoBetaN2(ii) = sum(sin(thetas).*..cTwoBeta0(taus(ii))/...
+        sum(sin(thetas)*ones(1, nPhi), 'all'), 'all');
+    cBeta(ii) = sum(cBeta0(taus(ii)), 'all');
+    cTwoBeta(ii) = sum(cTwoBeta0(taus(ii)), 'all');
 end
+
+figure(1)
+clf
+plot(taus, cBeta)
+hold on
+plot(taus, cTwoBeta)
+yyaxis right
+% plot(taus, cTwoBetaN2)
+% hold on
+% plot(taus, cTwoBeta/90)
+
+
+
+figure(2)
+clf
+plot(taus, cBetaN + cTwoBetaN)
